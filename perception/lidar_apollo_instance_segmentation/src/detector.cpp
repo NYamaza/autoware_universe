51aafef9dbdaa5e71ceab35329ba9f48db095128
NYamaza <hoge@hoge.hoge>
2026-02-18 23:15:59 +0900

    lidar_apollo_instance_segmentation buildエラー対応

--- perception/lidar_apollo_instance_segmentation/src/detector.cpp
+++ perception/lidar_apollo_instance_segmentation/src/detector.cpp
@@ -16,7 +16,7 @@
 
 #include "lidar_apollo_instance_segmentation/feature_map.hpp"
 
-#include <NvCaffeParser.h>
+//#include <NvCaffeParser.h>
 #include <NvInfer.h>
 #include <pcl_conversions/pcl_conversions.h>
 
